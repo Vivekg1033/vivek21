34bfadf76a1332f66865816f9e835626b58e3533
vivekg1033 <viveksgupta1033@gmail.com>
2024-09-27 23:09:17 +0530

    first commit

---
+++ src/main/java/com/gupta/Main.java
@@ -0,0 +1,7 @@
+package com.gupta;
+
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+    }
+}
---
+++ src/main/webapp/index.jsp
@@ -0,0 +1,5 @@
+<html>
+    <body>
+        <h1>welcome to vivek app </h1>
+    </body>
+</html>
